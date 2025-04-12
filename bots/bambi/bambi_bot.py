eab6e552a4d68d4e6c289241bf213f59ff0bfe46
StanGoleb <stanislawgoleb@gmai.com>
2025-04-13 01:15:03 +0200

    Updated the bambi bot

--- bots/bambi/bambi_bot.py
+++ bots/bambi/bambi_bot.py
@@ -1,71 +1,51 @@
 import os
-import re
-import random
-import time
-from datetime import date, timedelta
-import json
-import asyncio
+import sys
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
-from discord import SlashCommand
-from discord import ButtonStyle, Button
+from dotenv import load_dotenv
+import json
+import random
 
-load_dotenv('/tokens/bambi.env')
-DIS_TOKEN = os.getenv('DISCORD_TOKEN')
-CHAT_BOT_ID=int(os.getenv('BOT_ID'))
+# Przenieś się do katalogu pliku
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+project_root_dir = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
+if project_root_dir not in sys.path:
+    sys.path.append(project_root_dir)
 
-INTENTS = discord.Intents.all()
-INTENTS.messages = True
+load_dotenv('tokens.env')
+TOKEN = os.getenv('DISCORD_TOKEN')
+BOT_PREFIX = os.getenv('BOT_PREFIX', '@')
+BOT_NAME = os.getenv('BOT_NAME', 'Bambi')
+BOT_PERM = int(os.getenv('BOT_PERM', '0'))
 
+intents = discord.Intents.default()
+intents.message_content = True
 
-bot = commands.Bot(command_prefix='@', intents=INTENTS)
+activity = discord.Activity(type=discord.ActivityType.listening, name="BFF - bambi, Young Leosia, PG$, @atutowy")
 
-async def find_id(ctx, chat_name):
-        with open('../ids/chat_ids.json') as jfile:
-                chat_ids = json.load(jfile)
-        if chat_name in chat_ids:
-                return chat_ids[chat_name]
+class BambiBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=BOT_PREFIX, intents=intents, activity=activity)
+        self.bot_name = BOT_NAME
+        self.bot_prefix = BOT_PREFIX
+        self.dis_token = TOKEN
+        self.bot_perm = BOT_PERM
+        self.cog = None
 
-        else:
-                available_channels = ', '.join(chat_ids.keys())
-                err_msg = f'zła nazwa kanału: {chat_name} \n Dostepne nazwy to {available_channels}'
-                await ctx.send(err_msg)
-                return False
+    async def setup_hook(self):
+        await self.load_extension("bots.main_cogs")
+        self.cog = self.get_cog("MainCommands")
 
-async def check_permission(ctx, command):
-        with open(f'../permissions/{command}.json') as jfile:
-                permitted = json.load(jfile)
-                name = ctx.message.author.name
-                if name in permitted:
-                        return True
-                else:
-                        await ctx.send(f'Nie masz dostępu do komendy {command}')
-                        return False
+        @self.command(help="Wylosuj losowy wers piosenki")
+        async def wers(ctx):
+            with open("wers.json", encoding="utf-8") as jfile:
+                data = json.load(jfile)
+            with open("przywitania.json", encoding="utf-8") as jfile:
+                greetings = json.load(jfile)
+            random_song = random.choice(list(data.keys()))
+            random_line = random.choice(data[random_song])
+            random_greeting = random.choice(greetings)
+            await ctx.send(f"{random_greeting}\n{random_line}\n-Bambi, {random_song}")
 
-
-
-#================================================================================
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="BFF - bambi, Young Leosia, PG$, @atutowy"))
-
-@bot.command()
-async def wers(ctx):
-	with open('./wers.json', encoding='utf-8') as jfile:
-		data = json.load(jfile)
-	with open('./przywitania.json', encoding='utf-8') as jfile:
-		greetings = json.load(jfile)
-	random_song = random.choice(list(data.keys()))
-	random_line = random.choice(data[random_song])
-	random_greeting = random.choice(greetings)
-	await ctx.send(f"{random_greeting}\n{random_line}\n-Bambi, {random_song}")
-
-@bot.command()
-async def czatuj(ctx, message, chat_name='druciaki'):
-        await check_permission(ctx, 'czatuj')
-        curr_room = bot.get_channel(await find_id(ctx, chat_name))
-        if curr_room:
-                await curr_room.send(message)
-
-bot.run(DIS_TOKEN, reconnect=True)
+bot = BambiBot()
+bot.run(TOKEN)
